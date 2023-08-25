448c7a66e1620ea93505aaa844b37d1e30bd17e1
aelsayed <aelsayed@DBSF237.dbresults.local>
2023-08-25 16:05:39 +1000

    annotations update

--- TutorialMainTest/src/main/java/org/apache/struts/helloworld/action/HelloWorldAction.java
+++ TutorialMainTest/src/main/java/org/apache/struts/helloworld/action/HelloWorldAction.java
@@ -1,23 +1,44 @@
 package org.apache.struts.helloworld.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Namespace("/")
 public class HelloWorldAction extends ActionSupport {
+
     private List<String> sports = new ArrayList<>();
     private String newSport;
 
-    // Getter and setter for sports
+    public List<String> getSports() {
+        return sports;
+    }
 
-    // Getter and setter for newSport
+    public void setNewSport(String newSport) {
+        this.newSport = newSport;
+    }
 
+    @Action(value = "updateSports", results = {
+            @Result(name = "success", location = "/helloWorld.jsp"),
+            @Result(name = "input", location = "/helloWorld.jsp")
+    })
     public String updateSports() {
         if (newSport != null && !newSport.isEmpty()) {
             sports.add(newSport);
+            newSport = null; // Clear the input field
         }
         return SUCCESS;
     }
 
-    // Other methods...
+    @Action(value = "helloWorld", results = {
+            @Result(name = "success", location = "/helloWorld.jsp"),
+            @Result(name = "input", location = "/helloWorld.jsp")
+    })
+    public String helloWorld() {
+        return SUCCESS;
+    }
 }
--- TutorialMainTest/src/main/webapp/helloWorld.jsp
+++ TutorialMainTest/src/main/webapp/helloWorld.jsp
@@ -7,20 +7,24 @@
 <body>
     <h1>Hello World Example</h1>
     
-    <s:form action="helloWorld">
-        <s:textfield name="personBean.name" label="Name" value="%{personBean.name}" />
-        <s:textfield name="personBean.sport" label="Sport" value="%{personBean.sport}" />
-        <s:submit value="Submit" />
+    <s:form action="updateSports">
+        <s:textfield name="newSport" label="New Sport" value="%{newSport}" />
+        <s:submit value="Add Sport" />
     </s:form>
 
     <!-- Display validation errors -->
-    <s:fielderror fieldName="personBean.name" />
-    <s:fielderror fieldName="personBean.sport" />
+    <s:fielderror fieldName="newSport" />
 
-    <h2>Sports List:</h2>
-    <s:iterator value="sports">
-        <s:property />
-        <br />
-    </s:iterator>
+    <s:if test="sports.isEmpty()">
+        <div class="empty-message">No sports added yet.</div>
+    </s:if>
+    <s:else>
+        <!-- Display sports list -->
+        <h2>Sports List:</h2>
+        <s:iterator value="sports">
+            <s:property />
+            <br />
+        </s:iterator>
+    </s:else>
 </body>
 </html>
